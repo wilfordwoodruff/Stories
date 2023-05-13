93f9bb94644e6dc54b9e3318bdb44456cc5cdba6
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2023-05-12 19:43:22 -0500

    Update load_data.py

--- data/load_data.py
+++ data/load_data.py
@@ -5,7 +5,7 @@ import argparse
 
 def get_data(url, api_key, directory):
     headers = {
-        "Authorization": f"Bearer {api_key}"
+        "Authorization": api_key"
     }
 
     response = requests.get(url, headers=headers)
